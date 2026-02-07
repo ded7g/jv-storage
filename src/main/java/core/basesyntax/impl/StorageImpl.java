6a95d4833b0901b760aab05bb8951055a22be3b2
mc704ed <mykyta.churylov@student.tuke.sk>
2026-02-07 17:09:00 +0100

    update

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -3,19 +3,24 @@ package core.basesyntax.impl;
 import core.basesyntax.Storage;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
-    private Object[] keys = new Object[10];
-    private Object[] values = new Object[10];
-    private int size = 0;
+    private static final int CAPACITY = 10;
+    private K[] keys;
+    private V[] values;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public StorageImpl() {
+        keys = (K[]) new Object[CAPACITY];
+        values = (V[]) new Object[CAPACITY];
+        size = 0;
+    }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
-                values[i] = value;
-                return;
-            }
-        }
-        if (size < keys.length) {
+        int index = indexOfKey(key);
+        if (index >= 0) {
+            values[index] = value;
+        } else if (size < CAPACITY) {
             keys[size] = key;
             values[size] = value;
             size++;
@@ -24,16 +29,21 @@ public class StorageImpl<K, V> implements Storage<K, V> {
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < size; i++) {
-            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
-                return (V) values[i];
-            }
-        }
-        return null;
+        int index = indexOfKey(key);
+        return index >= 0 ? values[index] : null;
     }
 
     @Override
     public int size() {
         return size;
+    }
+
+    private int indexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
