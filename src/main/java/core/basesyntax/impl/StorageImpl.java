cd91ec5b19864a0d152f70549ea910a166110674
mc704ed <mykyta.churylov@student.tuke.sk>
2026-02-07 17:03:50 +0100

    update

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -2,18 +2,40 @@ package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
 
+import java.util.Arrays;
+
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private Object[] keys = new Object[10];
+    private Object[] values = new Object[10];
+    private int size = 0;
+
     @Override
     public void put(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                values[i] = value;
+                return;
+            }
+        }
+        if (size < keys.length) {
+            keys[size] = key;
+            values[size] = value;
+            size++;
+        }
     }
 
     @Override
     public V get(K key) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i] == key || (key != null && key.equals(keys[i]))) {
+                return (V) values[i];
+            }
+        }
         return null;
     }
 
     @Override
     public int size() {
-        return -1;
+        return size;
     }
 }
